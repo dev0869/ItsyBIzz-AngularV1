9574090dfcbc3c6b33610e365302f6581eb37794
dev0869 <deveshbisht36@gmail.com>
2024-05-06 23:09:09 -0500

    not found task added

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, } from '@angular/router';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [FormsModule ,RouterLink,RouterModule],
+  imports: [FormsModule ,RouterLink],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
 })
