e33fc98fd834f81e3ca2e45f11560a6bce7dd7c9
dev0869 <deveshbisht36@gmail.com>
2024-05-08 07:58:09 -0500

    fix:enquiry page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { SearchbarComponent } from '../../components/searchbar/searchbar.component';
+import { RouterLink } from '@angular/router';
+import { Observable, map, toArray } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent, CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-getProducts=inject(ApiService)
-ngOnInit(){
-this.getProducts.get('products').subscribe((data)=>console.log(data))
-}
+  getProducts = inject(ApiService);
+  products$!: Observable<any>;
+  ngOnInit() {
+    this.products$ = this.getProducts.get('products').pipe(
+      map((data: any[]) => data.slice(0, 3).reverse()), 
+    )
+    this.products$.subscribe();
+  }
 }
